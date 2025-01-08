502c86d1cacc5c7562b86a61ba81e837730ff296
sejin <neokin7734@naver.com>
2025-01-08 18:35:34 +0900

    2025-01-08 개인연습 async/await promise

--- privateTest/corsTest.js
+++ privateTest/corsTest.js
@@ -1,73 +1,76 @@
 const btn = document.querySelector(".btn");
-
 const indexBtn = document.querySelector(".indexBtn");
-
 const result = document.querySelector(".result");
+ 
+const API_URL = 'http://127.0.0.1:8080';
 
-// console.log(indexBtn.value);     // /api/index
-// console.log(indexBtn.getValue);  // undefined
-// console.log(indexBtn.getvalue);  // undefined
-// console.log(indexBtn.getAttribute(value)); //corsTest.js:10 Uncaught ReferenceError: value is not defined
+// 깨달음 
+//  비동기 함수를 사용하고 promise객체를 받지않으려면 전제조건이
+//    1. 비동기 처리 함수를 사용한 곳에서 async / await를 사용하거나
+//    2. async / await를 사용하지 않으면 then을 사용해야한다
 
-btn.addEventListener("click", (e)=>{
+btn.addEventListener("click", async (e)=>{
   e.preventDefault();
-  console.log("클릭 확인");
   
-  let test = fetch("http://127.0.0.1:8080/api/index");
+  let test = fetch( `${API_URL}/api/index`);
   console.log( test );
-  let jsondata = test.then( res => {
-          res.json().then( answer => {
-              console.log( answer.index);
-            }) //크아아아아아!!! 무슨 중첩이냐 JS!!!!!
-    // console.log( res.index );
-    // console.log("res : " + res);
-    //                       console.log("res.index : " + res.index );
-                          return res;
-                      })
-                      .catch( (error) => {
-                        // return new Error("응답오류")
-                        console.log("error 발생 : " + error.message );
-                       });
-  // console.log("jsondata : " + jsondata);
-  // console.log("jsondata : " + jsondata.index);
-  // console.log();
+  let jsondata = await test.then( res => { return res.json() })
+                     .then( res => {
+                      console.log( " btn : res : " + res.index);
+                      return res.index //문자열로 넘기게 해놨는데 여기도 프로미스객체로 리턴하네
+                     } )
+                     .catch(error => {return error.message} );
 
+                     
+  let addEl = document.createElement('p');
+  addEl.textContent = "결과 : " + jsondata;
 
+  result.appendChild(addEl);
 });
 
-indexBtn.addEventListener("click" , (e)=>{
+// fetch는 promise객체를 반환하고
+// fetch로부터 반환받은 promise객체의 json() 메서드를 사용해야
+// promise객체의 promise객체를 받환받을수있고
+// promise객체의 promise객체에서 then을 사용해야 반환된 인자를 json문자열. js 객체로 활용할수음..
+
+indexBtn.addEventListener("click" , async (e)=>{  // 함수에 async 추가
   e.preventDefault();
   console.log("버튼의 value값으로 API 호출 테스트");
-  let addUrl = indexBtn.value
+  // let addUrl = indexBtn.value;
 
-  // 반환받은 res.json()이 Promise 객체기 때문에 .then() 필요
-  let res = getData(`http://127.0.0.1:8080${indexBtn.value}`)
-           .then( res => { 
-            console.log( res );
-            console.log("indexBtn.addEventListner getData then res : " + res ); 
-            console.log(" : " + res.index ); 
-            return res;
-           })
-           .catch( (error) => {
-            // return new Error("응답오류")
-            console.log("error 발생 : " + error.message );
-           });
-
-  console.log("res : " + res);
-        
+  let res = await getData( API_URL + e.target.value );  // await 추가
   
-  // console.log(res.json());
- 
+  console.log("indexBtn : " ,res); //분명히 getData에서는 문자열로 반환을 하게 해놨는데
+                                    //받아보니 promise객체야.
+                                    // 익명함수에 async 추가와 getData 사용시 await추가로
+                                    // 정상동작
+
+  let addEl = document.createElement('p');
+  addEl.textContent = "결과 : " + res;
+
+  result.appendChild(addEl);
 })
 
 
 async function getData( url = "" ){
   try{
     let response = await fetch( url );
+
     if(!response.ok){
       throw new Error("응답코드 오류 : " + response.status);
     }
-    return await response.json(); // Promise 객체를 반환함
+
+    // .json() 메서드는 Promise 객체를 반환함
+    let res = await response.json()
+                      .then( res => {
+                        console.log(res);
+                        console.log(res.index);
+                        return res.index;
+                      });
+                      
+    console.log("res : " + res); // 문자열로 리턴을 제대로 받았는데 왜 리턴이 안넘어가냐
+    console.log(res);
+    return res;
   } catch ( error ){
     console.error("fetch로 잡을수있는 error는 네트워크 오류뿐");
     throw error;
